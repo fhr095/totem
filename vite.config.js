16fe199710588d308158f662cf836551eed598cd
fhr095 <143469663+fhr095@users.noreply.github.com>
2024-08-15 20:48:16 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/talkwithifc': {
-        target: 'https://roko.flowfuse.cloud',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/talkwithifc/, '')
-      }
-    }
-  },
   build: {
-    chunkSizeWarningLimit: 1000, // Aumenta o limite de tamanho do chunk para 1000 kB
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom']  // Separa React e ReactDOM em um chunk próprio
+          vendor: ['react', 'react-dom']
         }
       }
     }
